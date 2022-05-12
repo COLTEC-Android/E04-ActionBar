d55d9e0639ba6491d6b501f966e1fb7a7377943d
Samuel <samuel@gmail.com>
2022-05-12 20:07:43 -0300

    Android E04: ActionBar - Lidando com a ActionBar - Entendendo o Up Navigation - UpNavigation != Back

--- app/src/main/java/br/ufmg/coltec/e04_actionbar/SecondActivity.java
+++ app/src/main/java/br/ufmg/coltec/e04_actionbar/SecondActivity.java
@@ -3,7 +3,10 @@ package br.ufmg.coltec.e04_actionbar;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button btnupdade = findViewById(R.id.btn_update);
+        btnupdade.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
+                startActivity(intent);
+            }
+        }
+        );
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
