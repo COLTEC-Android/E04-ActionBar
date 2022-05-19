96d3aeaefcd045f6b74d552517698559becf35b0
BrendaSales <brendasales1997@gmail.com>
2022-05-18 21:36:21 -0300

    Finalização do exercício E04: ActionBar

--- app/src/main/java/br/ufmg/coltec/e04_actionbar/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/e04_actionbar/MainActivity.java
@@ -1,10 +1,16 @@
 package br.ufmg.coltec.e04_actionbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.core.view.MenuItemCompat;
+import androidx.appcompat.widget.SearchView;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -22,6 +28,77 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
         });
+
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                Toast.makeText(MainActivity.this, "Buscar o texto: " + s, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+        ShareActionProvider shareProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setPackage("com.whatsapp");
+        intent.setType("text/*");
+
+        intent.putExtra(Intent.EXTRA_TEXT, "Texto para enviar");
+
+        shareProvider.setShareIntent(intent);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        switch (id) {
+            case R.id.action_refresh:
+                Toast.makeText(this, "Clicou no refresh", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_about:
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+
+                alertBuilder.setIcon(R.drawable.ic_launcher_foreground);
+                alertBuilder.setTitle("About");
+                alertBuilder.setMessage("Aplicativo desenvolvido por Brenda Sales - Aluna de desenvolvimento de sistema do COLTEC/UFMG");
+
+                alertBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(getBaseContext(), "Fechou o about", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                AlertDialog dialog = alertBuilder.create();
+                dialog.show();
+                return true;
+
+            case R.id.action_logout:
+                this.finishAffinity();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
